c2309a11f1922bed1162c2be1dd91cb22bcfe2e5
jeeravan <pranaypandit12@gmail.com>
2026-06-03 04:59:25 +0530

    fix: dotenv override=False so shell/subprocess env wins over .env

--- nova_manager/core/config.py
+++ nova_manager/core/config.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(os.getcwd(), ".env"), override=True)
+load_dotenv(os.path.join(os.getcwd(), ".env"), override=False)
 
 
 DATABASE_URL = getenv("DATABASE_URL") or ""
